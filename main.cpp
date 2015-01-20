d4f361abf8a4ed5a70bb8fca3764635b116d21d2
metalgeekcz <zhangcheng@n155-p242.kthopen.kth.se>
2015-01-20 16:22:38 +0100

    add GPL

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+/*
+ITML C++
+Cheng Zhang, chengz@kth.se 
+
+(C) Copyright 2015, Cheng Zhang 
+
+This is free software, you can redistribute it and/or modify it under the terms of the GNU General Public License.
+
+The GNU General Public License does not permit this software to be redistributed in proprietary programs.
+
+This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 #include <iostream>
 #include "itml.h"
 #include "crossKNN.h"
